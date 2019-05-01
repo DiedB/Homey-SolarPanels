2d463200b0ea6210e141d10830c71fa271b51f4c
Rob <44335102+pro-sumer@users.noreply.github.com>
2019-05-01 21:46:41 +0200

    #14 Review rework: use indent size of 4 spaces (and add .eslintrc config to verify)

--- drivers/growatt/api.js
+++ drivers/growatt/api.js
@@ -13,28 +13,28 @@ const dataURL = `${baseURL}/newPlantAPI.do?action=getUserCenterEnertyData`
 let cookies
 
 function post (url, body) {
-  return fetch(url, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded', Cookie: cookies },
-    body: body
-  })
+    return fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded', Cookie: cookies },
+        body: body
+    })
 }
 
 function getCookies (response) {
-  return response.headers.getAll('Set-Cookie').map(directive => directive.split(';')[0]).join(';')
+    return response.headers.getAll('Set-Cookie').map(directive => directive.split(';')[0]).join(';')
 }
 
 function md5c (message) {
-  return md5(message).replace(/0(.)/g, 'c$1')
+    return md5(message).replace(/0(.)/g, 'c$1')
 }
 
 exports.login = async function (username, password) {
-  cookies = undefined
-  const response = await post(loginURL, `userName=${username}&password=${md5c(password)}`)
-  cookies = getCookies(response)
-  return response
+    cookies = undefined
+    const response = await post(loginURL, `userName=${username}&password=${md5c(password)}`)
+    cookies = getCookies(response)
+    return response
 }
 
 exports.getProduction = function () {
-  return post(dataURL, 'language=1')
+    return post(dataURL, 'language=1')
 }
--- drivers/growatt/driver.js
+++ drivers/growatt/driver.js
@@ -7,38 +7,38 @@ const Homey = require('homey')
 const api = require('./api')
 
 class Growatt extends Homey.Driver {
-  onPair (socket) {
-    let username
-    let password
-    let devices = []
+    onPair (socket) {
+        let username
+        let password
+        let devices = []
 
-    socket.on('login', async (data, callback) => {
-      username = data.username
-      password = data.password
-      try {
-        const loginResponse = await api.login(username, password)
-        const loginData = await loginResponse.json()
-        if (loginData.back.success) {
-          for (let inverter of loginData.back.data) {
-            devices.push({
-              name: inverter.plantName,
-              data: { id: inverter.plantId },
-              settings: { username, password }
-            })
-          }
-          callback(null, true)
-        } else {
-          callback(new Error(Homey.__('login_error')))
-        }
-      } catch (error) {
-        callback(new Error(Homey.__('network_error')))
-      }
-    })
+        socket.on('login', async (data, callback) => {
+            username = data.username
+            password = data.password
+            try {
+                const loginResponse = await api.login(username, password)
+                const loginData = await loginResponse.json()
+                if (loginData.back.success) {
+                    for (let inverter of loginData.back.data) {
+                        devices.push({
+                            name: inverter.plantName,
+                            data: { id: inverter.plantId },
+                            settings: { username, password }
+                        })
+                    }
+                    callback(null, true)
+                } else {
+                    callback(new Error(Homey.__('login_error')))
+                }
+            } catch (error) {
+                callback(new Error(Homey.__('network_error')))
+            }
+        })
 
-    socket.on('list_devices', (data, callback) => {
-      callback(null, devices)
-    })
-  }
+        socket.on('list_devices', (data, callback) => {
+            callback(null, devices)
+        })
+    }
 }
 
 module.exports = Growatt
