f30e71ca04774b4b770d29bf3040532bbe53a097
RoyK <rkokk@protonmail.com>
2021-11-24 21:51:58 +0100

    fix[apsystem]: missing error handling

--- drivers/apsystems/device.js
+++ drivers/apsystems/device.js
@@ -18,6 +18,24 @@ class APsystemsECUR extends Inverter {
         const ecur = new apsystems.ECUR(settings.ip, 8899);
         ecur.getECUdata(function(err, result) {
 
+            // Handle error
+            if (err ) {
+                this.log(`Unavailable (${error})`);
+                this.setUnavailable(`Error retrieving data (${error})`);
+
+                return;
+            }
+
+            // Verify availability
+            if (!this.getAvailable()) {
+                this.setAvailable().then(_ => {
+                    this.log('Available');
+                }).catch(error => {
+                    this.error('Setting availability failed');
+                })
+            }
+
+            // Set capabilities
             self.setCapabilityValue('meter_power', result.today_energy);
             self.setCapabilityValue('measure_power', result.current_power);
 
--- drivers/apsystems/driver.js
+++ drivers/apsystems/driver.js
@@ -10,7 +10,7 @@ class APsystems extends Homey.Driver {
 
             const ecur = new apsystems.ECUR(device.settings.ip, 8899);
             ecur.getECUdata(function(err, data) {
-                if (err !== null) callback(err);
+                if (err !== null) return callback(new Error(Homey.__('ip_error')));
                 callback(null, true);
             });
         });
