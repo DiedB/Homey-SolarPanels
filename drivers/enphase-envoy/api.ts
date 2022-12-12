695f8c8f3d5f5352b4f837ab88632e21a2fff942
Diederik Bakker <diederik@outlook.com>
2022-12-12 13:12:59 +0100

    Downgrade node-fetch to v2

--- drivers/enphase-envoy/api.ts
+++ drivers/enphase-envoy/api.ts
@@ -1,4 +1,4 @@
-import fetch from "node-fetch";
+const fetch = require("node-fetch");
 import http from "node:http";
 import https from "node:https";
 
--- drivers/tigo/api.ts
+++ drivers/tigo/api.ts
@@ -1,4 +1,4 @@
-import fetch from "node-fetch";
+const fetch = require("node-fetch");
 
 import { SummaryResponse, SystemsResponse } from "./types";
 
